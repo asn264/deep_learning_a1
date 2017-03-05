42931858523a1dcca3af824bb6476b9d29faf071
ass502 <ass502@nyu.edu>
2017-03-05 11:19:25 -0500

    architecture changes

--- mnist_pytorch.py
+++ mnist_pytorch.py
@@ -177,7 +177,7 @@ test_loader = torch.utils.data.DataLoader(testset, batch_size=len(testset), shuf
 #                    ])),
 #     batch_size=args.batch_size, shuffle=True, **kwargs)
 
-class Net(nn.Module):
+"""class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
         self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
@@ -203,8 +203,43 @@ class Net(nn.Module):
         x = F.relu(self.fc1(x))
         x = F.dropout(x, training=self.training)
         x = F.relu(self.fc2(x))
-        return F.log_softmax(x)
 
+        return F.log_softmax(x)"""
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
+        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+        self.conv3 = nn.Conv2d(20, 30, kernel_size=5)
+        self.conv1_bn = nn.BatchNorm2d(10)
+        self.conv2_bn = nn.BatchNorm2d(20)
+        self.conv3_bn = nn.BatchNorm2d(30)
+        self.conv2_drop = nn.Dropout2d()
+        self.conv3_drop = nn.Dropout2d()
+        self.fc1 = nn.Linear(320, 50)
+        self.fc2 = nn.Linear(50, 10)
+
+        #Xavier weight initialization
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+            elif isinstance(m, nn.Linear):
+                n_in = m.in_features
+                n_out = m.out_features
+                m.weight.data.normal_(0, math.sqrt(2. / (n_in + n_out)))
+
+    def forward(self, x):
+        x = F.relu(F.max_pool2d(self.conv1_bn(self.conv1(x)), 2))
+        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2_bn(self.conv2(x))), 2))
+        #x = F.relu(F.max_pool2d(self.conv3_drop(self.conv3_bn(self.conv3(x))), 2))
+        x = x.view(-1, 320)
+        x = F.relu(self.fc1(x))
+        x = F.dropout(x, training=self.training)
+        x = F.relu(self.fc2(x))
+        
+        return F.log_softmax(x)
 
 model = Net()
 if args.cuda:
@@ -264,8 +299,8 @@ for epoch in range(1, args.epochs + 1):
     #orig_accs.append(c_orig_acc)
 
 #plt.plot(np.arange(args.epochs), orig_accs, marker='o', label='Orig Accuracy')
-plt.plot(np.arange(args.epochs), dev_accs, marker='o', label='Validation Accuracy')
+"""plt.plot(np.arange(args.epochs), dev_accs, marker='o', label='Validation Accuracy')
 plt.plot(np.arange(args.epochs), train_accs, marker='o', label='Train Accuracy')
 plt.title('MNIST: Train and Validation Accuracies')
 plt.legend(loc='upper left')
-plt.savefig('accuracies_with_distortion.jpg')
+plt.savefig('accuracies_with_distortion.jpg')"""
